21062707399a8e3358807cf2a76426e63c18994b
HyeongTaekJo <diajd169@gmail.com>
2023-12-12 16:24:42 +0900

    형택

---
+++ src/main/java/egovframework/example/sample/controller/htController.java
@@ -0,0 +1,11 @@
+package egovframework.example.sample.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class htController {
+	
+}
