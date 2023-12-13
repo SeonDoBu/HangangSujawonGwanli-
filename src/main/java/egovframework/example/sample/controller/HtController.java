c039efd13ed97aaf922ad1c4d78fe48d13212ca3
Mdragon <minyong_e89@naver.com>
2023-12-13 13:54:29 +0900

    [test] git-htController

---
+++ src/main/java/egovframework/example/sample/controller/HtController.java
@@ -0,0 +1,34 @@
+package egovframework.example.sample.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import egovframework.example.sample.service.SiseolService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class HtController {
+
+		private final SiseolService siseolService;
+		
+		@RequestMapping("test.do")
+		public String test(Model model) {
+			System.out.println("htController Start...");
+			try {
+				int result = siseolService.count();
+				System.out.println("htController result--> " + result);
+				
+				model.addAttribute("result", result);
+			}catch (Exception e) {
+				System.out.println("htController Exception -> " + e.getMessage());
+			}
+			
+			return "siseolTest";
+		}
+		
+		
+		
+		
+	}
+
