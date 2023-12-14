880429da643c773273811f7698d2e30693e4e1be
skdhfl <skdhfl@gmail.com>
2023-12-14 15:51:34 +0900

    [add] commoncode / district
    mapper, service 등 추가

---
+++ src/main/java/egovframework/example/sample/service/impl/CommoncodeServiceImpl.java
@@ -0,0 +1,15 @@
+package egovframework.example.sample.service.impl;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
+import egovframework.example.sample.service.CommoncodeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CommoncodeServiceImpl extends EgovAbstractServiceImpl implements CommoncodeService {
+
+}
---
+++ src/main/java/egovframework/example/sample/service/impl/DistrictServiceImpl.java
@@ -0,0 +1,15 @@
+package egovframework.example.sample.service.impl;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
+import egovframework.example.sample.service.DistrictService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DistrictServiceImpl extends EgovAbstractServiceImpl implements DistrictService {
+
+}
