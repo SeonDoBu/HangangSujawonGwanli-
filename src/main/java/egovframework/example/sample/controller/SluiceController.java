561d2b7827c7a22b724517feae73042907392c82
skdhfl <skdhfl@gmail.com>
2023-12-14 16:07:05 +0900

    [add] 실시간수문관리 수자원
    jsp 및 controller method 추가

--- src/main/java/egovframework/example/sample/controller/SluiceController.java
+++ src/main/java/egovframework/example/sample/controller/SluiceController.java
@@ -1,6 +1,7 @@
 package egovframework.example.sample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +9,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SluiceController {
 
+	@RequestMapping(value = "sujawonList.do")
+	public String sujawonList() {
+		return "sujawonList";
+	}
+	
+	@RequestMapping(value = "sujawonDetail.do")
+	public String sujawonDetail() {
+		return "sujawonDetail";
+	}
+	
+	@RequestMapping(value = "sujawonInsertForm.do")
+	public String sujawonInsertForm() {
+		return "sujawonInsert";
+	}
+	
+	@RequestMapping(value = "sujawonInsert.do")
+	public String sujawonInsert() {
+		return "sujawonList";
+	}
+	
+	@RequestMapping(value = "sujawonUpdate.do")
+	public String sujawonUpdate() {
+		return "sujawonDetail";
+	}
+	
+	@RequestMapping(value = "sujawonDelete.do")
+	public String sujawonDelete() {
+		return "sujawonList";
+	}
+	
+	@RequestMapping(value = "sujawonStatistics.do")
+	public String sujawonStatistics() {
+		return "sujawonStatistics";
+	}
 }
